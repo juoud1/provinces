296413b459beaf5c877da2b9cb9a1ca38319c260
Dongongo <juoud1@gmail.com>
2019-12-03 00:11:06 -0500

    update dummy api

--- src/main/java/com/dobatii/dockerization1/controller/ProvinceController.java
+++ src/main/java/com/dobatii/dockerization1/controller/ProvinceController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dobatii.dockerization1.data.Province;
 import com.dobatii.dockerization1.service.ProvinceService;
 
-import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
